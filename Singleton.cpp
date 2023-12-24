8d3c8b9694e448f5cd37e728803c858d2d3fd2d3
elkyzeer <maksimkykla12@icluod.com>
2023-12-24 15:32:18 +0200

    Singleton.cpp

---
+++ Singleton.cpp
@@ -0,0 +1,19 @@
+﻿class Singleton {
+private:
+    static Singleton* instance;
+
+    // Private constructor to prevent instantiation
+    Singleton() {}
+
+public:
+    // Static method to get the instance of the Singleton class
+    static Singleton* getInstance() {
+        if (instance == nullptr) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+};
+
+// Initializing the instance to nullptr
+Singleton* Singleton::instance = nullptr;
