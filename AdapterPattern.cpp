092b4249c4856964fc465e366e9a4903e482e2f8
elkyzeer <maksimkykla12@icluod.com>
2023-12-24 15:18:19 +0200

    AdapterPattern.cpp

---
+++ AdapterPattern.cpp
@@ -0,0 +1,45 @@
+﻿// Target interface
+class Target {
+public:
+    virtual void request() = 0;
+};
+
+// Adaptee class
+class Adaptee {
+public:
+    void specificRequest() {
+        // Perform specific request
+    }
+};
+
+// Adapter class
+class Adapter : public Target {
+private:
+    Adaptee* adaptee;
+
+public:
+    Adapter(Adaptee* adaptee) : adaptee(adaptee) {}
+
+    void request() override {
+        // Call the specific request method of the adaptee
+        adaptee->specificRequest();
+    }
+};
+
+// Client code
+int main() {
+    // Create an instance of the Adaptee class
+    Adaptee* adaptee = new Adaptee();
+
+    // Create an instance of the Adapter class, passing the Adaptee object
+    Target* adapter = new Adapter(adaptee);
+
+    // Call the request method of the Target interface, which internally calls the specificRequest method of the Adaptee
+    adapter->request();
+
+    // Clean up
+    delete adapter;
+    delete adaptee;
+
+    return 0;
+}
