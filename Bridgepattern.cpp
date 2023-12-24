6f5036c0ebfe37eb82b4bbba08b943f49fe533ab
elkyzeer <maksimkykla12@icluod.com>
2023-12-24 15:08:41 +0200

    Bridgepattern.cpp

---
+++ Bridgepattern.cpp
@@ -0,0 +1,55 @@
+﻿// Abstraction class
+class Abstraction {
+protected:
+    class Implementation* implementation;
+public:
+    virtual void operation() = 0;
+};
+
+// Implementation class
+class Implementation {
+public:
+    virtual void performOperation() = 0;
+};
+
+// Concrete Abstraction class
+class ConcreteAbstraction : public Abstraction {
+public:
+    void operation() {
+        implementation->performOperation();
+    }
+};
+
+// Concrete Implementation class
+class ConcreteImplementationA : public Implementation {
+public:
+    void performOperation() {
+        // Perform operation A
+    }
+};
+
+// Concrete Implementation class
+class ConcreteImplementationB : public Implementation {
+public:
+    void performOperation() {
+        // Perform operation B
+    }
+};
+
+int main() {
+    Abstraction* abstraction = new ConcreteAbstraction();
+    Implementation* implementationA = new ConcreteImplementationA();
+    Implementation* implementationB = new ConcreteImplementationB();
+
+    abstraction->implementation = implementationA;
+    abstraction->operation();
+
+    abstraction->implementation = implementationB;
+    abstraction->operation();
+
+    delete abstraction;
+    delete implementationA;
+    delete implementationB;
+
+    return 0;
+}
