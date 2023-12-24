9eb21bcd00764ce34ddd29d69b71ee67ea639595
elkyzeer <maksimkykla12@icluod.com>
2023-12-24 15:29:40 +0200

    Prototypepattern.cpp

---
+++ Prototypepattern.cpp
@@ -0,0 +1,25 @@
+﻿// Prototype base class
+class Prototype {
+public:
+    virtual Prototype* clone() = 0;
+};
+
+// Concrete prototype class
+class ConcretePrototype : public Prototype {
+public:
+    Prototype* clone() override {
+        return new ConcretePrototype(*this);
+    }
+};
+
+int main() {
+    // Create an instance of the concrete prototype
+    ConcretePrototype prototype;
+
+    // Clone the prototype
+    Prototype* clonedPrototype = prototype.clone();
+
+    // Use the cloned prototype
+
+    return 0;
+}
